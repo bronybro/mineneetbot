f26ffce464861a11a4791baa7137da05bebab25e
bronybro <bronybro@tutanota.com>
2020-07-29 04:33:52 +0000

    clear and help commands

--- main.py
+++ main.py
@@ -11,7 +11,62 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
+bot.remove_command('help')
 start_time = datetime.utcnow()
+
+
+@bot.event
+async def on_member_join(member):  # say "Hello" when someone join GUILD
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Привет, {member.name}! Добро пожаловать на наш ламповый сервер!')
+
+
+@bot.event
+async def on_ready():  # set bot ptesence
+    for guild in bot.guilds:
+        if guild.name == GUILD:
+            break
+    print(f'{bot.user.name} has connected to Discord server {guild.name}(id: {guild.id})  {start_time}\n')
+    await bot.change_presence(activity=discord.Game("кости | !help - F.A.Q."))
+
+
+@bot.event
+async def on_member_update(before, after):  # send in text channel information about members status changes
+    for guild in bot.guilds:
+        category = guild.categories[0]
+        channel = category.channels[4]
+    if str(before.status) == "online" or "offline":
+        status_msg = "{} has gone {}!".format(after.name, after.status)
+        await channel.send(status_msg)
+
+
+@bot.command(name='help', pass_context=True)
+@commands.has_permissions(administrator=True)
+async def help_command(ctx):  # give list of bot commands
+    command_prefix = '!'
+    emb = discord.Embed(title=' F.A.Q.:clipboard: ')
+    emb.description = "Список команд бота"
+    emb.add_field(name='{}clear :broom: '.format(command_prefix), value='Очистка чата (Админ)')
+    emb.add_field(name='{}d6 :game_die: '.format(command_prefix), value='Roll D6')
+    emb.add_field(name='{}d20 :game_die: '.format(command_prefix), value='Roll D20')
+    emb.add_field(name='{}uptime :timer: '.format(command_prefix), value='Аптайм бота')
+    emb.add_field(name='{}donate :moneybag: '.format(command_prefix), value='Поддержать проект')
+    await ctx.send(embed=emb)
+
+
+@bot.command(name='clear', pass_context=True)
+@commands.has_permissions(administrator=True)
+async def clear(ctx, amount: int):  # clear chat
+    await ctx.channel.purge(limit=amount + 1)
+
+
+@clear.error
+async def clear_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'{ctx.author.name}, укажите аргумент (количество удаляемых сообщений)!')
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'{ctx.author.name}, не достаточно прав для выполнения данной команды!')
 
 
 @bot.command(name="uptime", pass_context=True)
@@ -36,34 +91,8 @@ async def donate(ctx):  # give link to Donate
     await ctx.send(file=discord.File('donate.png'))
 
 
-@bot.event
-async def on_member_join(member):  # say "Hello" when someone join GUILD
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Привет, {member.name}! Добро пожаловать на наш ламповый сервер!')
-
-
-@bot.event
-async def on_ready():  # set bot ptesence
-    for guild in bot.guilds:
-        if guild.name == GUILD:
-            break
-    print(f'{bot.user.name} has connected to Discord server {guild.name}(id: {guild.id})  {start_time}\n')
-    await bot.change_presence(activity=discord.Game("кости"))
-
-
-@bot.event
-async def on_member_update(before, after):  # send in rexr channel information about membes status changes
-    for guild in bot.guilds:
-        category = guild.categories[0]
-        channel = category.channels[1]
-    if str(before.status) == "online" or "offline":
-        status_msg = "{} has gone {}!".format(after.name, after.status)
-        await channel.send(status_msg)
-
-
 @bot.command(name='d6', pass_context=True)
-async def d6(ctx):  # roll d6 dice
+async def d6_roll(ctx):  # roll d6 dice
     d6_list = ['d6/dice1.png',
                'd6/dice2.png',
                'd6/dice3.png',
@@ -75,7 +104,7 @@ async def d6(ctx):  # roll d6 dice
 
 
 @bot.command(name='d20', pass_context=True)
-async def d20(ctx):  # roll d20 dice
+async def d20_roll(ctx):  # roll d20 dice
     d20_list = ['d20/dice1.png', 'd20/dice6.png', 'd20/dice11.png', 'd20/dice16.png',
                 'd20/dice2.png', 'd20/dice7.png', 'd20/dice12.png', 'd20/dice17.png',
                 'd20/dice3.png', 'd20/dice8.png', 'd20/dice13.png', 'd20/dice18.png',
